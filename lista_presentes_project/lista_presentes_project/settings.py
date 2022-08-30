d91c8dc1c7e1835be3e09adb2b59fee2a962a6a3
GustavoPerezYSSY <gustavo.perez@yssy.com.br>
2022-08-30 15:22:19 -0300

    fix/theme

--- lista_presentes_project/lista_presentes_project/settings.py
+++ lista_presentes_project/lista_presentes_project/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'presentes',
         'USER': 'postgres',
-        'PASSWORD': 'h_9ijUty',
-        'HOST': 'host.docker.internal',
+        'PASSWORD': 'spk9punx',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
